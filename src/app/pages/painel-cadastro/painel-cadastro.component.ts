99f7efbb66e160d5b60d18e178ab6c104c9b5e5f
Flávio Peres <flaviopc2@gmail.com>
2023-10-21 13:24:38 -0300

    Primeiro commit na nova branch

--- src/app/pages/painel-cadastro/painel-cadastro.component.ts
+++ src/app/pages/painel-cadastro/painel-cadastro.component.ts
@@ -8,6 +8,22 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class PainelCadastroComponent implements OnInit {
 
+  formularioCadastroVisivel = false;
+  cadastro: any = {};
+
+  mostrarFormularioCadastro() {
+    this.formularioCadastroVisivel = true;
+  }
+
+  enviarCadastro() {
+    // Lógica para enviar o cadastro à sua API
+    // Depois de enviar o cadastro, você pode redefinir o formulário e ocultá-lo
+    console.log('Dados do cadastro:', this.cadastro);
+    // Reinicialize o formulário após o envio
+    this.formularioCadastroVisivel = false;
+    this.cadastro = {};
+  }
+
 
   constructor() {
 
