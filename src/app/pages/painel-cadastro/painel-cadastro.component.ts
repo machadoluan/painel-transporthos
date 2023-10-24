8294413e00f314d09f68e98247b31e089faec687
Flávio Peres <flaviopc2@gmail.com>
2023-10-24 07:51:05 -0300

    deixando o popup de cadastro responsivo

--- src/app/pages/painel-cadastro/painel-cadastro.component.ts
+++ src/app/pages/painel-cadastro/painel-cadastro.component.ts
@@ -11,32 +11,6 @@ import { CadastroPopupComponent } from '../cadastro-popup/cadastro-popup.compone
 })
 export class PainelCadastroComponent implements OnInit {
 
-  formularioCadastroVisivel = false;
-  cadastro: any = {};
-
-  mostrarFormularioCadastro() {
-    this.formularioCadastroVisivel = true;
-  }
-
-  enviarCadastro() {
-    // Lógica para enviar o cadastro à sua API
-    // Depois de enviar o cadastro, você pode redefinir o formulário e ocultá-lo
-    console.log('Dados do cadastro:', this.cadastro);
-    // Reinicialize o formulário após o envio
-    this.formularioCadastroVisivel = false;
-    this.cadastro = {};
-  }
-
-
-  formatDateInput() {
-    let value = this.cadastro.data;
-    value = value.replace(/\D/g, ''); // Remova todos os caracteres não numéricos
-    if (value.length >= 2) {
-      value = value.replace(/(\d{2})(\d{2})(\d{4})/, '$1/$2/$3');
-    }
-    this.cadastro.data = value;
-  }
-
   constructor() {
 
   }
@@ -47,7 +21,7 @@ export class PainelCadastroComponent implements OnInit {
     const url = '/cadastro';
 
     // Configuração da janela pop-up (tamanho, nome, etc.)
-    const configuracao = 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1300, height=350';
+    const configuracao = 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, width=1300, height=350,resizable=no';
 
     // Abra a janela pop-up
     window.open(url, 'CadastroPopup', configuracao);
